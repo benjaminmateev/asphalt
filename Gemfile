77b1d05419edd2cad74f028ce61b2654238e7182
Benjamin Mateev <benmateev@gmail.com>
2013-10-02 08:42:38 +0200

    adding unicorn to the equation

--- Gemfile
+++ Gemfile
@@ -14,7 +14,7 @@ gem 'mysql2',       '~> 0.2.13'
 # Deploy with Capistrano
 gem 'capistrano'
 gem 'compass',      ">= 0.11.5"
-
+gem 'unicorn'
 # To use debugger (ruby-debug for Ruby 1.8.7+, ruby-debug19 for Ruby 1.9.2+)
 # gem 'ruby-debug'
 # gem 'ruby-debug19', :require => 'ruby-debug'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,12 @@
+require "bundler/capistrano"
+
+default_run_options[:pty] = true
 set :application, "asphalt-berlin.com"
 
-set :scm, :none
-set :repository, "."
+set :scm, :git
+set :repository, "git@github.com:benjaminmateev/asphalt.git"
+set :branch, "master"
+set :deploy_via, :remote_cache
 set :deploy_via, :copy
 
 set :user, :root
@@ -10,13 +15,28 @@ role :web, "198.211.120.82"                          # Your HTTP server, Apache/
 role :app, "198.211.120.82"                          # This may be the same as your `Web` server
 role :db,  "198.211.120.82", :primary => true # This is where Rails migrations will run
 
-set :deploy_to,   "/home/#{application}"
+set :deploy_to,   "/home/deploy/#{application}"
 
 set :use_sudo, false
-ssh_options[:paranoid] = false
+set :ssh_options, { :forward_agent => true, :paranoid => true }
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
+
+namespace :deploy do
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
+    end
+  end
+
+  # task :setup_config, roles: :app do
+  #   sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+  #   sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+  # end
+  # after "deploy:setup", "deploy:setup_config"
+end
 
 # If you are using Passenger mod_rails uncomment this:
 namespace :configure do
@@ -26,21 +46,10 @@ namespace :configure do
   task :set_db_link do
     run "ln -s #{shared_path}/db/production.sqlite3 #{current_release}/db/production.sqlite3"
   end
-  task :set_permissions do
-    run "chown -R www-data:www-data #{deploy_to}"
-  end
-end
-
-namespace :apache do
-  task :restart do
-    run "/etc/init.d/apache2 restart"
-  end
 end
 
 # after deploy hooks
 after :deploy do
   #configure.set_config_link
   configure.set_db_link
-  configure.set_permissions
-  apache.restart
 end
